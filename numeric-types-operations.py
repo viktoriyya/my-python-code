8b4e15acc527b62516166eb504a0321a4a3c88ca
viktoriyya <viktoriya.baturina@icloud.com>
2024-09-26 13:45:34 +0200

    last task

--- numeric-types-operations.py
+++ numeric-types-operations.py
@@ -1,4 +1,4 @@
-import cmath
+import math
 
 a = 24 + 12
 print(a)
@@ -22,8 +22,23 @@ print(complex_number)
 g = True * False
 print(g)
 
-h = 37348 / 0 #devision by 0 
-print(h)
+#h = 37348 / 0 #devision by 0 
+#print(h)
 
 # type error
+
+# task 2 
+output = f"""
+max(4, 3.5, 5) = {max(4, 3.5, 5)}
+max(True, False, 0.9) = {max(True, False, 0.9)}
+min(94, 27, 45.25) = {min(94, 27, 45.25)}
+min(False, True, 1) = {min(False, True, 1)}
+abs(-58.234) = {abs(-58.234)}
+abs(3 + 4j) = {abs (3 + 4j)}
+pow(3, 7) = {pow(3, 7)}
+ceil(84 / 3) = {math.ceil(84 / 3)}
+ceil(2.84) = {math.ceil(2.83)}
+floor(78.9) = {math.floor(78.9)}
+"""
+print(output)
 
