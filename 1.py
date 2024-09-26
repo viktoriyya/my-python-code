3b7dda9f0d3874f9c4f0f78f77feecc9708a6b90
viktoriyya <viktoriya.baturina@icloud.com>
2024-09-26 02:38:28 +0200

    task 1

---
+++ 1.py
@@ -0,0 +1,18 @@
+import cmath
+
+# Integer
+print(123, "is type of", type(123))
+
+# Float
+print(43.23, "is type of", type(43.23))
+
+# Complex
+print(4 - 1j, "is type of", type(4 - 1j))
+
+# String
+print("How are you?", "is type of", type("How are you?"))
+
+# Boolean
+print(True, "is type of", type(True))
+
+
