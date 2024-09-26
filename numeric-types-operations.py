e60ff2ff7737520d967d79373467479d5266fa77
viktoriyya <viktoriya.baturina@icloud.com>
2024-09-26 13:16:34 +0200

    first commit

---
+++ numeric-types-operations.py
@@ -0,0 +1,29 @@
+import cmath
+
+a = 24 + 12
+print(a)
+b = 85 - 83
+print(b)
+c = 8 * 32
+print(c)
+d = 16 / 4
+print(d)
+e = 2 ** 10
+print(e)
+f = 30 % 7
+print(f)
+
+# complex numbers
+
+
+complex_number = 3 + 2j
+print(complex_number)
+
+g = True * False
+print(g)
+
+h = 37348 / 0 #devision by 0 
+print(h)
+
+# type error
+
