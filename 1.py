a45483eb94eed0ac0b4c1af904c711506262e6bd
viktoriyya <viktoriya.baturina@icloud.com>
2024-09-26 03:02:29 +0200

    task 3

--- 1.py
+++ 1.py
@@ -9,10 +9,16 @@ print(43.23, "is type of", type(43.23))
 # Complex
 print(4 - 1j, "is type of", type(4 - 1j))
 
-# String
+ # String
 print("How are you?", "is type of", type("How are you?"))
 
 # Boolean
 print(True, "is type of", type(True))
 
+
+print(isinstance(123, int))
+print(isinstance(43.23, int))
+print(isinstance(4 - 1j, complex))
+print(isinstance("How are you?", str))
+print(isinstance(True, str))
 
